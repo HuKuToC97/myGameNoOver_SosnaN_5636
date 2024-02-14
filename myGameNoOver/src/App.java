c2d40ab2d0dc3d5ddb858b76b31c3e49e96c813e
NiKiTa <hukuta199701@gmail.com>
2024-02-14 22:12:16 +0300

    Break program on Java

--- myGameNoOver/src/App.java
+++ myGameNoOver/src/App.java
@@ -1,12 +1,21 @@
 import сreatures.Animal;
-import сreatures.Unit;
 import сreatures.Auxiliary.Location2D;
 
 public class App {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
 
-        Animal kapibara = new Animal("Константин", "Капибара", 100, 1000000, 1000000, new Location2D(), 100, 100, 50,
-                150, 100);
-                System.out.println(kapibara);
+        Animal kapibara = new Animal("Константин", 
+        "Капибара", 
+        100, 
+        1000000,       
+        1000000, 
+        new Location2D(), 
+        100, 
+        100, 50, 150, 100);
+                System.out.println(kapibara.toString());
+                System.out.println("123");
     }
 }
