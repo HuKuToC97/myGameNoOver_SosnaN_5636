47a43387af3675c0965c5f4f3129ea253c0bbc10
NiKiTa <hukuta199701@gmail.com>
2024-02-25 21:54:39 +0300

    Add setters and getters in class Location2D

--- myGameNoOver/src/Unit/Auxiliary/Location2D.java
+++ myGameNoOver/src/Unit/Auxiliary/Location2D.java
@@ -1,28 +1,47 @@
 package Unit.Auxiliary;
 
 public class Location2D {
-    int x;
-    int y;
+    private int x;
+    private int y;
 
-
-    public Location2D(int x, int y){
-    this.x = x;
-    this.y = y;
+    public Location2D(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public Location2D(){
+    public Location2D() {
         this(0, 0);
     }
 
     @Override
     public String toString() {
-        
+
         return "x: " + x + ", y: " + y;
     }
-    
+
     public double calculateDistance(Location2D other) {
         int deltaX = this.x - other.x;
         int deltaY = this.y - other.y;
         return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    }
+
+    //
+    // Getters and setters
+    //
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
